50bdd21316f6796f238118f2cf604b805342cd95
Pedro Gabriel <pedrogabriellima@gmail.com>
2025-04-10 14:19:15 -0300

    feat(login): adicionar validador, redirecionamento de rota e password type

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -5,6 +5,7 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatButtonModule } from '@angular/material/button';
 import { FormBuilder, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { LogoComponent } from '../../shared/components/logo/logo.component';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -23,16 +24,18 @@ import { LogoComponent } from '../../shared/components/logo/logo.component';
 })
 export class LoginComponent {
   formBuilder: FormBuilder = inject(FormBuilder);
+  router: Router = inject(Router);
   passwordPattern: RegExp = /^(?=.*[A-Z])(?=.*[!@#$%^&*(),.?":{}|<>]).{8,}$/;
 
   loginForm = this.formBuilder.group({
-    user: [null, Validators.required],
+    user: [null, [Validators.required, Validators.minLength(4)]],
     password: [null, [Validators.required, Validators.pattern(this.passwordPattern)]]
   })
 
   onSubmit(): void {
     if(this.loginForm.valid) {
-      console.log(this.loginForm.value)
+      // console.log(this.loginForm.value)
+      this.router.navigate(['/dashboard'])
     }    
   }
 }
