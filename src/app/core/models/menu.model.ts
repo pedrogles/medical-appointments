835193366cbe58a4046f07933dc701a4668a9459
Pedro Gabriel <pedrogabriellima@gmail.com>
2025-04-10 14:09:54 -0300

    feat(menu): adicionar ids aos itens do menu

--- src/app/core/models/menu.model.ts
+++ src/app/core/models/menu.model.ts
@@ -1,12 +1,13 @@
 export interface IMenuItem {
+    id: number;
     label: string;
     route: string;
     icon?: string;
 }
 
 export const menuItems: IMenuItem[] = [
-    { label: 'Dashboard', route: '/', icon: 'home' },
-    { label: 'Cadastro de Paciente', route: '/', icon: 'person_add' },
-    { label: 'Agendamento de Consulta', route: '/', icon: 'add_circle' },
-    { label: 'Gerenciar Pacientes/Consultas', route: '/', icon: 'list'}
+    { id: 0, label: 'Dashboard', route: '/', icon: 'home' },
+    { id: 1, label: 'Cadastro de Paciente', route: '/cadastro', icon: 'person_add' },
+    { id: 2, label: 'Agendamento de Consulta', route: '/agendamento', icon: 'add_circle' },
+    { id: 3, label: 'Gerenciar Pacientes/Consultas', route: '/gerenciamento', icon: 'list'}
 ]
