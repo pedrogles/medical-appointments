f8598c483656ce727548ac2946aa163aa4fc3e05
Pedro Gabriel <pedrogabriellima@gmail.com>
2025-04-14 13:04:31 -0300

    feat(auth): criar layout base para páginas de autenticação

---
+++ src/app/layout/auth-layout/auth-layout/auth-layout.component.ts
@@ -0,0 +1,13 @@
+import { Component, input } from '@angular/core';
+import { LogoComponent } from '../../../shared/components/logo/logo.component';
+
+@Component({
+  selector: 'app-auth-layout',
+  standalone: true,
+  imports: [LogoComponent],
+  templateUrl: './auth-layout.component.html',
+  styleUrl: './auth-layout.component.scss'
+})
+export class AuthLayoutComponent {
+  title = input.required<string>();
+}
