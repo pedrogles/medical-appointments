94752be16af81c21819840b18580b876fe086b35
Pedro Gabriel <pedrogabriellima@gmail.com>
2025-05-05 23:01:31 -0300

    refactor(menu): altera rotas das páginas de dashboard e cadastro de paciente

--- src/app/core/constants/menu.constant.ts
+++ src/app/core/constants/menu.constant.ts
@@ -1,8 +1,8 @@
 import { IMenuItem } from "../interfaces/menu.interface";
 
 export const menuItems: IMenuItem[] = [
-    { id: 0, label: 'Dashboard', route: '/', icon: 'home' },
-    { id: 1, label: 'Cadastro de Paciente', route: '/cadastro', icon: 'person_add' },
+    { id: 0, label: 'Dashboard', route: '/dashboard', icon: 'home' },
+    { id: 1, label: 'Cadastro de Paciente', route: '/cadastro-de-paciente', icon: 'person_add' },
     { id: 2, label: 'Agendamento de Consulta', route: '/agendamento', icon: 'add_circle' },
     { id: 3, label: 'Gerenciar Pacientes/Consultas', route: '/gerenciamento', icon: 'list'}
 ]
