b5b929f74349e2e7889a62db472b5168038b01c3
Pedro Gabriel <pedrogabriellima@gmail.com>
2025-05-05 21:51:18 -0300

    feat(libs): adiciona chart.js para gráficos, ngx-mask para mascaramento de input e json-server para simulação de API

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,17 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideCharts, withDefaultRegisterables } from 'ng2-charts';
+import { provideHttpClient } from '@angular/common/http';
+import { provideNgxMask } from 'ngx-mask';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimationsAsync()]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }), 
+    provideRouter(routes), 
+    provideAnimationsAsync(), 
+    provideCharts(withDefaultRegisterables()),
+    provideHttpClient(),
+    provideNgxMask()
+  ]
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,14 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { PatientRegistrationComponent } from './pages/patient-registration/patient-registration.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'login', pathMatch: 'full' },
     { path: 'login', component: LoginComponent },
     { path: 'register', component: RegisterComponent },
+    { path: 'dashboard', component: DashboardComponent },
+    { path: 'cadastro-de-paciente', component: PatientRegistrationComponent },
     { path: '**', redirectTo: 'login' }
 ];
