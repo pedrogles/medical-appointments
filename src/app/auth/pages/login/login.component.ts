29080285f2a6ff359dce45bbf79114aefdf282ca
Pedro Gabriel <pedrogabriellima@gmail.com>
2025-05-11 22:07:49 -0300

    feat(login): adiciona mensagens de erro nos inputs, condicional de loading no submit e aprimora o modelo de rotas para navegação no fluxo de login.

--- src/app/auth/pages/login/login.component.ts
+++ src/app/auth/pages/login/login.component.ts
@@ -3,22 +3,27 @@ import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatButtonModule } from '@angular/material/button';
 import { FormBuilder, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
-import { AuthService } from '../service/auth.service';
-import { IUser } from '../../core/interfaces/user.interface';
-import { AuthLayoutComponent } from '../../layout/auth-layout/auth-layout.component';
+import { AuthService } from '../../services/auth.service';
+import { IUser } from '../../../core/interfaces/user.interface';
+import { AuthLayoutComponent } from '../../../layout/auth-layout/auth-layout.component';
 import { Router, RouterLink } from '@angular/router';
+import { SpinnerComponent } from '../../../shared/components/spinner/spinner.component';
+import { CommonModule } from '@angular/common';
+import { finalize } from 'rxjs';
 
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [
+    CommonModule,
     FormsModule, 
     MatFormFieldModule, 
     MatInputModule, 
     MatButtonModule, 
     ReactiveFormsModule,
     AuthLayoutComponent,
-    RouterLink
+    RouterLink,
+    SpinnerComponent
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
@@ -28,7 +33,8 @@ export class LoginComponent {
   private readonly formBuilder = inject(FormBuilder);
   private readonly authService = inject(AuthService);
   private readonly router = inject(Router);
-
+  
+  loading = false;
   loginForm = this.formBuilder.group({
     email: ['', [Validators.required, Validators.email]],
     password: ['', [Validators.required, Validators.pattern(this.passwordPattern)]]
@@ -38,16 +44,27 @@ export class LoginComponent {
     if(this.loginForm.valid) {
       const { email, password } = this.loginForm.value;
       const user = { email, password } as IUser;
-      this.authService.login(user).subscribe({
+      this.loading = true;
+      this.authService.login(user)
+      .pipe(finalize(() => this.loading = false))
+      .subscribe({
         next: () => {
           // Adicionar toast de sucesso de login
-          this.router.navigateByUrl("/dashboard");
+          this.router.navigate(["app/dashboard"]);
         },
         error: (error) => {
           // Adicionar toast de erro de login
           console.log(error)
         }
-      });
+      })
     }    
+  }
+
+  handleRedirectToRegister(): void {
+    this.router.navigate(["auth/register"]);
+  }
+
+  get formControls() {
+    return this.loginForm.controls;
   }
 }
