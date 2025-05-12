cdc81a5c7696d2a5aad853c313d6692ad708964b
Pedro Gabriel <pedrogabriellima@gmail.com>
2025-05-11 22:14:53 -0300

    feat(register): adiciona condicional de loading, realiza logout após criação do usuário, redireciona para tela de login, mapeia mensagens de erro e aprimora fluxo de navegação no cadastro.

--- src/app/auth/pages/register/register.component.ts
+++ src/app/auth/pages/register/register.component.ts
@@ -1,15 +1,17 @@
 import { Component, inject } from '@angular/core';
 import { FormBuilder, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
-import { AuthService } from '../service/auth.service';
+import { AuthService } from '../../services/auth.service';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
-import { AuthLayoutComponent } from '../../layout/auth-layout/auth-layout.component';
+import { AuthLayoutComponent } from '../../../layout/auth-layout/auth-layout.component';
 import { Router, RouterLink } from '@angular/router';
 import { CommonModule } from '@angular/common';
-import { IUser } from '../../core/interfaces/user.interface';
-import { passwordMatchValidator } from '../../shared/validators/passwordMatch.validator';
+import { IUser } from '../../../core/interfaces/user.interface';
+import { passwordMatchValidator } from '../../../shared/validators/passwordMatch.validator';
+import { SpinnerComponent } from '../../../shared/components/spinner/spinner.component';
+import { finalize, switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-register',
@@ -23,7 +25,8 @@ import { passwordMatchValidator } from '../../shared/validators/passwordMatch.va
       MatButtonModule, 
       ReactiveFormsModule,
       AuthLayoutComponent,
-      RouterLink
+      RouterLink,
+      SpinnerComponent
     ],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
@@ -34,6 +37,7 @@ export class RegisterComponent {
   private readonly authService = inject(AuthService);
   private readonly router = inject(Router);
 
+  loading = false;
   registerForm = this.formBuilder.group({
     username: ['', [Validators.required, Validators.minLength(4)]],
     email: ['', [Validators.required, Validators.email]],
@@ -45,17 +49,26 @@ export class RegisterComponent {
     if(this.registerForm.valid) {
       const { username, email, password } = this.registerForm.value;
       const user = { username, email, password } as IUser;
-      this.authService.register(user).subscribe({
+       this.loading = true;
+       this.authService.register(user)
+        .pipe(
+          switchMap(() => this.authService.logout()),
+          finalize(() => this.loading = false))
+        .subscribe({
         next: () => {
           // Inserir toast de confirmação de cadastro.
-          this.router.navigateByUrl("/login");
+          this.handleRedirectToLogin();
         },
         error: (error) => {
           // Inserir toast de erro.
-          console.log(error)
+          console.error(error)
         }
       });
     }    
+  }
+
+  handleRedirectToLogin(): void {
+    this.router.navigate(["auth/login"]);
   }
 
   get formControls() {
