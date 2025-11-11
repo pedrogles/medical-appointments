eee91fd106dca1b5ede7ce917814e8cbee54205b
Pedro Gabriel <pedrogabriellima@gmail.com>
2025-11-10 23:06:13 -0300

    feat(auth): add auth layout component

---
+++ src/app/features/auth/layout/auth-layout/auth-layout.component.ts
@@ -0,0 +1,12 @@
+import { Component, input } from '@angular/core';
+
+@Component({
+  selector: 'app-auth-layout',
+  standalone: true,
+  imports: [],
+  templateUrl: './auth-layout.component.html',
+  styleUrl: './auth-layout.component.scss'
+})
+export class AuthLayoutComponent {
+  title = input.required<string>();
+}
