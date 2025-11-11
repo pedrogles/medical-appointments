2d8fb00c19dd8f2ff56c321985b82a386e8c5b11
Pedro Gabriel <pedrogabriellima@gmail.com>
2025-11-11 14:43:59 -0300

    feat(core): enable async animations support

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,6 +2,7 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
 import { provideAuth, getAuth } from '@angular/fire/auth';
@@ -11,6 +12,7 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }), 
     provideRouter(routes),
+    provideAnimationsAsync(),
     provideFirebaseApp(() => initializeApp(environments.firebaseConfig)),
     provideAuth(() => getAuth())
   ]
