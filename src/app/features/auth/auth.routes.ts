9f10bc57c1737739c8cec6a960925114461443eb
Pedro Gabriel <pedrogabriellima@gmail.com>
2025-11-10 23:12:57 -0300

    feat(auth): create auth routes with lazy-loading

---
+++ src/app/features/auth/auth.routes.ts
@@ -0,0 +1,24 @@
+import { Routes } from "@angular/router";
+import { LoginComponent } from "./pages/login/login.component";
+import { RegisterComponent } from "./pages/register/register.component";
+
+export const AUTH_ROUTES: Routes = [
+    {
+        path: '',
+        redirectTo: 'login',
+        pathMatch: 'full'
+    },
+    { 
+        path: 'login', 
+        component: LoginComponent, 
+    },
+    { 
+        path: 'register', 
+        component: RegisterComponent, 
+    },
+    {
+        path: '**',
+        redirectTo: 'login'
+    }
+
+];
