01d6c2b4d5ac42aed5a856b3da500bb1959f1a44
Pedro Gabriel <pedrogabriellima@gmail.com>
2025-12-16 12:11:04 -0300

    feat(auth): refactor auth service and improve error handling

--- src/app/features/auth/service/auth.service.ts
+++ src/app/features/auth/service/auth.service.ts
@@ -1,50 +1,35 @@
 import { inject, Injectable } from '@angular/core';
-import { Auth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, updateProfile } from '@angular/fire/auth';
-import { from, Observable } from 'rxjs';
+import { Auth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, updateProfile, UserCredential } from '@angular/fire/auth';
+import { catchError, from, map, Observable, switchMap, throwError } from 'rxjs';
 import { IUser } from '../../../core/interfaces/user.interface';
+import { FirebaseErrorService } from '../../../core/services/firebase-error/firebase-error.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
   private readonly firebaseAuth = inject(Auth);
+  private readonly firebaseError = inject(FirebaseErrorService);
 
   login(user: IUser): Observable<void> {
-    const promise = signInWithEmailAndPassword(this.firebaseAuth, user.email, user.password)
-      .then(async (response) => {
-        try {
-          const accessToken = await response.user.getIdToken(true);
-          localStorage.setItem("accessToken", accessToken);
-        } catch(tokenError) {
-          console.error("Erro ao obter Token: ", tokenError)
-        }
-      })
-      .catch((error) => {
-        console.error("Erro ao fazer login: ", error)
-        throw error
-      })
-      return from(promise);
+    return from(signInWithEmailAndPassword(this.firebaseAuth, user.email, user.password)).pipe(
+      map(() => void 0),
+      catchError((error) => throwError(() => this.firebaseError.translate(error.code)))
+    );
   }
 
   register(user: IUser): Observable<void> {
-    const promise = createUserWithEmailAndPassword(this.firebaseAuth, user.email, user.password)
-      .then(async (response) => {
-        await updateProfile(response.user, { displayName: user.username});
-      })
-      .catch((error) => {
-        console.error("Erro ao cadastrar novo usuário: ", error)
-        throw error
-      })
-    return from(promise);
+    return from(createUserWithEmailAndPassword(this.firebaseAuth, user.email, user.password)).pipe(
+      switchMap((response: UserCredential) => from(updateProfile(response.user, { displayName: user.username }))),
+      map(() => void 0),
+      catchError((error) => throwError(() => this.firebaseError.translate(error.code)))
+    );
   }
 
   logout(): Observable<void> {
-    const promise = signOut(this.firebaseAuth)
-      .then(() => localStorage.removeItem("accessToken"))
-      .catch((error) => {
-        console.error("Erro ao realizar logout: ", error)
-        throw error
-      })
-    return from(promise);
+    return from(signOut(this.firebaseAuth)).pipe(
+      map(() => void 0),
+      catchError((error) => throwError(() => "Erro ao realizar logout. Tente novamente."))
+    );
   }
 }
