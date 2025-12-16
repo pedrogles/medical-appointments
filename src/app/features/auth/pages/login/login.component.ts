666501e5e1af502c57601a4ec8686da0bc0edb20
Pedro Gabriel <pedrogabriellima@gmail.com>
2025-12-16 12:18:12 -0300

    feat(auth): implement login flow with validation and feedback

--- src/app/features/auth/pages/login/login.component.ts
+++ src/app/features/auth/pages/login/login.component.ts
@@ -2,10 +2,17 @@ import { Component, inject } from '@angular/core';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatButtonModule } from '@angular/material/button';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { FormBuilder, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { AuthLayoutComponent } from '../../layout/auth-layout/auth-layout.component';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
+import { AuthService } from '../../service/auth.service';
+import { IUser } from '../../../../core/interfaces/user.interface';
+import { REGEX } from '../../../../core/constants/regex';
+import { finalize } from 'rxjs';
+
+import { ToastService } from '../../../../core/services/toast/toast.service';
 
 @Component({
   selector: 'app-login',
@@ -18,28 +25,51 @@ import { Router } from '@angular/router';
     MatButtonModule, 
     ReactiveFormsModule,
     AuthLayoutComponent,
+    MatProgressSpinnerModule
   ],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
   private readonly formBuilder = inject(FormBuilder);
+  private readonly authService = inject(AuthService);
   private readonly router = inject(Router);
+  private readonly toastService = inject(ToastService);
   
+  loading: boolean = false;
   loginForm = this.formBuilder.group({
     email: ['', [Validators.required, Validators.email]],
-    password: ['', [Validators.required]]
+    password: ['', [Validators.required, Validators.pattern(REGEX.password)]]
   })
 
   handleLogin(): void {
     if(this.loginForm.valid) {
       const { email, password } = this.loginForm.value;
-      console.log(`Email:${email}, Senha:${password}`);
-    }    
+      const user = { email, password } as IUser;
+      this.loading = true;
+      this.authService.login(user)
+      .pipe(finalize(() => this.loading = false))
+      .subscribe({
+        next: () => {
+          this.handleRedirect('dashboard');
+          this.toastService.show('Login realizado com sucesso!', 'success');
+        },
+        error: (errorMessage: string) => {
+          this.toastService.show(errorMessage, 'error');
+        }
+      })
+    }     
   }
 
-  handleRedirectToRegister(): void {
-    this.router.navigate(["auth/register"]);
+  handleRedirect(page: string): void {
+    switch(page) {
+      case 'dashboard':
+        this.router.navigate(["app/dashboard"]);
+        break;
+      case 'register':
+        this.router.navigate(["auth/register"]);
+        break;
+    }
   }
 
   get formControls() {
