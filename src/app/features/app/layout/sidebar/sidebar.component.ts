3ffe1fb5f2be170730bca00b54d29c9034273f7c
Pedro Gabriel <pedrogabriellima@gmail.com>
2026-01-05 21:54:19 -0300

    feat(app): add SidebarComponent with menu functionality and styles

---
+++ src/app/features/app/layout/sidebar/sidebar.component.ts
@@ -0,0 +1,33 @@
+import { Component, inject } from '@angular/core';
+import { MatDividerModule } from '@angular/material/divider';
+import { Router, RouterModule } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
+import { IMenuItem } from '../../../../core/interfaces/menu.interface';
+import { menuItems } from '../../../../core/constants/menu.constant';
+import { AuthService } from '../../../auth/service/auth.service';
+
+@Component({
+  selector: 'medical-sidebar',
+  standalone: true,
+  imports: [MatDividerModule, RouterModule, MatIconModule],
+  templateUrl: './sidebar.component.html',
+  styleUrl: './sidebar.component.scss'  
+})
+export class SidebarComponent {
+  menuItems: IMenuItem[] = menuItems;
+  private readonly authService = inject(AuthService);
+  private readonly router = inject(Router);
+
+  onLogout(): void {
+    this.authService.logout().subscribe({
+      next: () => {
+        // Adicionar toast de confirmação
+        this.router.navigate(["auth/login"]);
+      },
+      error: (error) => {
+        // Adicionar toast de erro
+        console.log(error)
+      }
+    })
+  }
+}
