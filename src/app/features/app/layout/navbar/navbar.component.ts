61938a8bcf333fbfa44ac92b01233f445312a320
Pedro Gabriel <pedrogabriellima@gmail.com>
2026-01-05 21:53:23 -0300

    feat(app): implement NavbarComponent with menu functionality and styles

---
+++ src/app/features/app/layout/navbar/navbar.component.ts
@@ -0,0 +1,48 @@
+import { Component, inject } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { animate, state, style, transition, trigger } from '@angular/animations';
+import { Router, RouterModule } from '@angular/router';
+import { MatDividerModule } from '@angular/material/divider';
+import { AuthService } from '../../../auth/service/auth.service';
+import { IMenuItem } from '../../../../core/interfaces/menu.interface';
+import { menuItems } from '../../../../core/constants/menu.constant';
+
+@Component({
+  selector: 'medical-navbar',
+  standalone: true,
+  imports: [MatIconModule, RouterModule, MatDividerModule],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.scss',
+  animations: [
+    trigger("openClose", [
+      state("open", style({ top: '4rem' })),
+      state("close", style({ top: '-25rem' })),
+      transition('open => close', [animate('1s ease-in')]),
+      transition('close => open', [animate('1s ease-out')])
+    ])
+  ]
+})
+export class NavbarComponent {
+  isMenu = false;
+  menuItems: IMenuItem[] = menuItems;
+
+  private readonly authService = inject(AuthService);
+  private readonly router = inject(Router);
+
+  onChangeMenuState(): void {
+    this.isMenu = !this.isMenu;
+  };
+
+  onLogout(): void {
+    this.authService.logout().subscribe({
+      next: () => {
+        // Adicionar toast de confirmação
+        this.router.navigate(["auth/login"]);
+      },
+      error: (error) => {
+        // Adicionar toast de erro
+        console.log(error)
+      }
+    })
+  }
+}
