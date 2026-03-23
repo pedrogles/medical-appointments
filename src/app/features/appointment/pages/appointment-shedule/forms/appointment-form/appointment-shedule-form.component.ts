7ba3a03930f0fdcbfede19a70c5d6873e97befa2
Pedro Gabriel <pedrogabriellima@gmail.com>
2026-03-23 10:49:22 -0300

    feat(appointment): enhance appointment form with autocomplete for patient and professional selection

--- src/app/features/appointment/pages/appointment-shedule/forms/appointment-form/appointment-shedule-form.component.ts
+++ src/app/features/appointment/pages/appointment-shedule/forms/appointment-form/appointment-shedule-form.component.ts
@@ -1,6 +1,6 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { NgxMaskDirective } from 'ngx-mask';
-import { CommonModule } from '@angular/common';
+import { AsyncPipe, CommonModule } from '@angular/common';
 import { FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
@@ -8,7 +8,18 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { ToastService } from '../../../../../../core/services/toast/toast.service';
+import { CreateAppointmentDTO } from '../../../../dtos/create-appointment.dto';
+import { AppointmentService } from '../../../../services/appointment.service';
+import { combineLatest, debounceTime, distinctUntilChanged, filter, Observable, switchMap } from 'rxjs';
+import { PatientService } from '../../../../../patient/services/patient.service';
+import { AppointmentReview, AppointmentTimeOptions } from '../../../../../../core/types/appointment.type';
+import { AppointmentReviewComponent } from '../../components/appointment-review/appointment-review/appointment-review.component';
+import { ProfessionalService } from '../../../../../professional/services/professional.service';
+import { IPatient } from '../../../../../../core/interfaces/patient.interface';
+import { IProfessional } from '../../../../../../core/interfaces/professional.interface';
+import { AppointmentFormType } from '../../../../../../core/types/appointmentForm.type';
 
 @Component({
   selector: 'medical-appointment-shedule-form',
@@ -23,88 +34,221 @@ import { ToastService } from '../../../../../../core/services/toast/toast.servic
     MatSelectModule,
     ReactiveFormsModule,
     MatProgressSpinnerModule,
-    NgxMaskDirective
+    NgxMaskDirective,
+    AppointmentReviewComponent,
+    MatAutocompleteModule,
+    AsyncPipe
   ],
   templateUrl: './appointment-shedule-form.component.html',
   styleUrl: './appointment-shedule-form.component.scss'
 })
 export class AppointmentSheduleFormComponent implements OnInit {
-  appointmentSheduleForm!: FormGroup<{
-    patientId: FormControl<string>;
-    professionalId: FormControl<string>;
-    date: FormControl<string>;
-    hour: FormControl<string>;
-    notes: FormControl<string>;
-  }>;
-  isLoading = true;
-  readonly appointmentTimeOptions = [ 
-    { label: '08:00', value: '08:00' }, 
-    { label: '09:00', value: '09:00' }, 
-    { label: '10:00', value: '10:00' }, 
-    { label: '11:00', value: '11:00' }, 
-    { label: '13:00', value: '13:00' }, 
-    { label: '14:00', value: '14:00' }, 
-    { label: '15:00', value: '15:00' }, 
-    { label: '16:00', value: '16:00' }, 
-    { label: '17:00', value: '17:00' }, 
-    { label: '18:00', value: '18:00' }, 
-    { label: '19:00', value: '19:00' }, 
-    { label: '20:00', value: '20:00' }
+  appointmentForm!: FormGroup<AppointmentFormType>;
+  isLoading = false;
+
+  appointmentReviewData: AppointmentReview = {
+    patientName: '',
+    patientCPF: '',
+    professionalName: '',
+    professionalSpecialty: '',
+    date: '',
+    time: '',
+    notes: ''
+  }; 
+
+  patients$!: Observable<IPatient[]>;
+  professionals$!: Observable<IProfessional[]>;
+
+  appointmentTimeOptions: AppointmentTimeOptions[] = [ 
+    { label: '08:00', value: '08:00', disabled: true }, 
+    { label: '09:00', value: '09:00', disabled: true }, 
+    { label: '10:00', value: '10:00', disabled: true }, 
+    { label: '11:00', value: '11:00', disabled: true }, 
+    { label: '13:00', value: '13:00', disabled: true }, 
+    { label: '14:00', value: '14:00', disabled: true }, 
+    { label: '15:00', value: '15:00', disabled: true }, 
+    { label: '16:00', value: '16:00', disabled: true }, 
+    { label: '17:00', value: '17:00', disabled: true }, 
+    { label: '18:00', value: '18:00', disabled: true }, 
+    { label: '19:00', value: '19:00', disabled: true }, 
+    { label: '20:00', value: '20:00', disabled: true }
   ];
     
   private readonly formBuilder = inject(FormBuilder);
   private readonly toast = inject(ToastService);
+  private readonly appointmentService = inject(AppointmentService);
+  private readonly patientService = inject(PatientService);
+  private readonly professionalService = inject(ProfessionalService);
 
   ngOnInit(): void {
     this.initializeForm();
-    setTimeout(() => {
-      this.isLoading = false;
-    }, 2000);
+    this.watchFormChanges();
   }
 
-  initializeForm(): void {
-    this.appointmentSheduleForm = this.formBuilder.nonNullable.group({
-      patientId: ['', [Validators.required]],
-      professionalId: ['', [Validators.required]],
-      date: ['', [Validators.required]],
-      hour: ['', [Validators.required]],
-      notes: ['', [Validators.maxLength(500)]]
+  private initializeForm(): void {
+    this.appointmentForm = this.formBuilder.group<AppointmentFormType>({
+      patient: new FormControl<IPatient | null>(null, Validators.required),
+      professional: new FormControl<IProfessional | null>(null, Validators.required),
+      date: new FormControl<string>('', { nonNullable: true, validators: [Validators.required] }),
+      hour: new FormControl<string>('', { nonNullable: true, validators: [Validators.required] }),
+      notes: new FormControl<string>('', { nonNullable: true, validators: [Validators.required, Validators.maxLength(500)] })
     });
   }
 
   onSubmit(): void {
     this.isLoading = true;
-    // const appointmentSheduleData: CreateAppointmentDTO = this.buildCreateAppointmentDTO();
-    setTimeout(() => {
-      this.toast.show(`Consulta agendada com sucesso!`, 'success');
-      this.isLoading = false;
-      this.appointmentSheduleForm.reset();
-    }, 2000); 
+    if (this.appointmentForm.valid) {
+      const appointmentDTO = this.buildCreateAppointmentDTO();
+      this.appointmentService.create(appointmentDTO).subscribe({
+        next: () => {
+          this.toast.show(`Consulta agendada com sucesso!`, 'success');
+          this.appointmentForm.reset();
+          this.appointmentReviewData = {
+            patientName: '',
+            patientCPF: '',
+            professionalName: '',
+            professionalSpecialty: '',
+            date: '',
+            time: '',
+            notes: ''
+          };
+          this.isLoading = false;
+        },
+        error: (error) => {
+          console.error('Error creating appointment:', error);
+          this.toast.show('Erro ao agendar consulta. Por favor, tente novamente.', 'error');
+          this.isLoading = false;
+        }
+      });
+    }
   }
 
-  // private buildCreateAppointmentDTO(): CreateAppointmentDTO {
-  //   const formValue = this.appointmentSheduleForm.getRawValue();
-  //   return {
-  //     patientId: formValue.patientId,
-  //     professionalId: formValue.professionalId,
-  //     startDateTime: this.startDateTime(formValue.date, formValue.hour),
-  //     notes: formValue.notes
-  //   };
-  // }
+  private buildCreateAppointmentDTO(): CreateAppointmentDTO {
+    const form = this.appointmentForm.getRawValue();
+    return {
+      patient_id: form.patient!.id,
+      professional_id: form.professional!.id,
+      start_datetime: this.startDateTime(form.date, form.hour),
+      notes: form.notes
+    };
+  }
 
-  // private startDateTime(date: string, hour: string): string {
-  //   const isoString = `${date}T${hour}:00`;
-  //   const parsed = new Date(isoString);
+  private startDateTime(date: string, hour: string): string {
+    const isoString = `${date}T${hour}:00`;
+    const parsed = new Date(isoString);
+    if (isNaN(parsed.getTime())) {
+      this.toast.show(`Data final inválida`, 'error');
+      throw new Error('Data final inválida');
+    }
+    return parsed.toISOString();
+  }
 
-  //   if (isNaN(parsed.getTime())) {
-  //     this.toast.show(`Data final inválida`, 'error');
-  //     throw new Error('Data final inválida');
-  //   }
+  private watchFormChanges(): void {
+    this.listenPatientSearch();
+    this.listenProfessionalSearch();
+    this.listenFormChanges();
+  }
 
-  //   return parsed.toISOString();
-  // }
+  private listenPatientSearch(): void {
+    const control = this.formControls.patient;
 
-  get formControls(): typeof this.appointmentSheduleForm.controls {
-      return this.appointmentSheduleForm.controls;
+    this.patients$ = control!.valueChanges.pipe(
+      debounceTime(500),
+      distinctUntilChanged(),
+      filter((value: string | IPatient | null): value is string =>
+        typeof value === 'string' && value.length >= 3
+      ),
+      switchMap(search => this.patientService.search(search))
+    );
+  }
+
+  private listenProfessionalSearch(): void {
+    const control = this.formControls.professional;
+
+    this.professionals$ = control!.valueChanges.pipe(
+      debounceTime(500),
+      distinctUntilChanged(),
+      filter((value: string | IProfessional | null): value is string =>
+        typeof value === 'string' && value.length >= 3
+      ),
+      switchMap(search => this.professionalService.search(search))
+    );
+  }
+
+  private listenFormChanges(): void {
+    combineLatest([
+      this.formControls.professional.valueChanges,
+      this.formControls.date.valueChanges
+    ])
+    .pipe(
+      debounceTime(500),
+      filter(([professional, date]) => !!professional && !!date),
+      distinctUntilChanged(([prevProf, prevDate], [currProf, currDate]) =>
+        prevProf?.id === currProf?.id && prevDate === currDate
+      )
+    )
+    .subscribe(([professional, date]) => {
+      this.getProfessionalAppointmentsByDate(
+        professional!.id,
+        date
+      );
+    });
+
+    this.appointmentForm.valueChanges
+    .pipe(debounceTime(500))
+    .subscribe(() => {
+      const form = this.appointmentForm.getRawValue();
+      this.appointmentReviewData = { 
+        ...this.appointmentReviewData,
+        date: form.date,
+        time: form.hour,
+        notes: form.notes
+      };
+    });
+  }
+
+  selectPatient(patient: IPatient): void {
+    this.appointmentReviewData = {
+      ...this.appointmentReviewData,
+      patientName: patient.name,
+      patientCPF: patient.cpf
+    };
+  }
+
+  selectProfessional(professional: IProfessional): void {
+    this.appointmentReviewData = {
+      ...this.appointmentReviewData,
+      professionalName: professional.name,
+      professionalSpecialty: professional.specialty
+    };
+  }
+
+  displayInputName(person: IPatient | IProfessional): string {
+    return person ? person.name : '';
+  }
+
+  private getProfessionalAppointmentsByDate(professionalId: string, date: string): void {
+    this.appointmentService
+    .getProfessionalAppointmentsByDate(
+      professionalId, 
+      date
+    )
+    .subscribe({
+      next: (appointments) => {
+        const bookedTimes = appointments.map((appt: any) => 
+          new Date(appt).toLocaleTimeString('pt-BR', { 
+            hour: '2-digit', 
+            minute: '2-digit' 
+          })
+        );
+        for (let option of this.appointmentTimeOptions) {
+          bookedTimes.includes(option.value) ? option.disabled = true : option.disabled = false;
+        }
+      }
+    });
+  }
+
+  get formControls(): typeof this.appointmentForm.controls {
+      return this.appointmentForm.controls;
   }
 }
