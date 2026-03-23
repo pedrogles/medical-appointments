31c0c9f0b0e5c5df3c7dfda6c7bd71d92d143ac2
Pedro Gabriel <pedrogabriellima@gmail.com>
2026-03-23 10:46:21 -0300

    fix(dto): revert property names in CreateAppointmentDTO to snake_case

--- src/app/features/appointment/dtos/create-appointment.dto.ts
+++ src/app/features/appointment/dtos/create-appointment.dto.ts
@@ -1,6 +1,6 @@
 export interface CreateAppointmentDTO {
-    patientId: string;
-    professionalId: string;
-    startDateTime: string; // ISO 8601 (ex: 2026-02-12T14:00:00)
+    patient_id: string;
+    professional_id: string;
+    start_datetime: string; // ISO 8601 (ex: 2026-02-12T14:00:00)
     notes: string;
 }
