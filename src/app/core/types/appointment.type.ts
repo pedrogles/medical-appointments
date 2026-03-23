53ce958b17fcc2dfc877fdb96b2d38ace243bc37
Pedro Gabriel <pedrogabriellima@gmail.com>
2026-03-23 10:45:53 -0300

    feat(types): add AppointmentReview type and extend appointment-related types

--- src/app/core/types/appointment.type.ts
+++ src/app/core/types/appointment.type.ts
@@ -1,1 +1,21 @@
+import { IAppointment } from "../interfaces/appointment.interface";
+
 export type AppointmentStatus = 'scheduled' | 'completed' | 'canceled' | 'no-show';
+
+export type AppointmentReview = {
+    patientName: string;
+    patientCPF: string;
+    professionalName: string;
+    professionalSpecialty: string;
+    date: string; 
+    time: string; 
+    notes: string;
+}
+
+export type AppointmentStartDateTime = IAppointment['startDateTime'];
+
+export type AppointmentTimeOptions = { 
+    label: string, 
+    value: string, 
+    disabled: boolean 
+}
