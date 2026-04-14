b0ed319f86801f6342dce710de71f9113ad61f66
Pedro Gabriel <pedrogabriellima@gmail.com>
2026-04-14 17:58:14 -0300

    feat(dashboard): add PageHeaderComponent to dashboard page

--- src/app/features/dashboard/pages/dashboard/dashboard.component.ts
+++ src/app/features/dashboard/pages/dashboard/dashboard.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { PageHeaderComponent } from '../../../../shared/components/page-header/page-header.component';
 
 @Component({
   selector: 'medical-dashboard',
   standalone: true,
-  imports: [],
+  imports: [
+    PageHeaderComponent
+  ],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
